c1475d3d2ad39e7e7e9cc3482681baa6810e7da1
Mfitumukiza Peter <61537587+Peter-Mfitumukiza@users.noreply.github.com>
2023-02-27 06:23:24 +0200

    removing containers

--- prisma/migrations/20230226195338_updated/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE "Account" ALTER COLUMN "statusId" SET DEFAULT 1,
-ALTER COLUMN "roleId" SET DEFAULT 3;
-
--- AlterTable
-ALTER TABLE "Community" ALTER COLUMN "statusId" SET DEFAULT 1;
-
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "statusId" SET DEFAULT 1;
---
+++ prisma/migrations/20230227040506_updated/migration.sql
@@ -0,0 +1,313 @@
+-- CreateTable
+CREATE TABLE "Media" (
+    "id" TEXT NOT NULL,
+    "media_typeId" TEXT NOT NULL,
+    "media_url" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "projectId" TEXT,
+    "achievementId" TEXT,
+
+    CONSTRAINT "Media_pkey" PRIMARY KEY ("id")
+)
+
+-- CreateTable
+CREATE TABLE "Media_type" (
+    "id" TEXT NOT NULL,
+    "media_type" TEXT NOT NULL,
+
+    CONSTRAINT "Media_type_pkey" PRIMARY KEY ("id")
+)
+
+-- CreateTable
+CREATE TABLE "Status" (
+    "id" SERIAL NOT NULL ,
+    "status" TEXT NOT NULL,
+
+    CONSTRAINT "Status_pkey" PRIMARY KEY ("id")
+)
+
+-- CreateTable
+CREATE TABLE "Role" (
+    "id" SERIAL NOT NULL,
+    "role" TEXT NOT NULL,
+
+    CONSTRAINT "Role_pkey" PRIMARY KEY ("id")
+)
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "names" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "country" TEXT NOT NULL,
+    "emailVerificationCode" TEXT,
+    "emailVerificationCodeExpiresAt" BIGINT,
+    "resetPasswordCode" TEXT,
+    "resetPasswordCodeExpiresAt" BIGINT,
+    "emailVerified" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "statusId" INTEGER NOT NULL DEFAULT 1,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+)
+
+-- CreateTable
+CREATE TABLE "Community" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "statusId" INTEGER NOT NULL DEFAULT 1,
+    "description" TEXT NOT NULL,
+    "profile" TEXT,
+    "creatorId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Community_pkey" PRIMARY KEY ("id")
+)
+
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "statusId" INTEGER NOT NULL DEFAULT 1,
+    "roleId" INTEGER NOT NULL DEFAULT 3,
+    "communityId" TEXT NOT NULL,
+    "profile" TEXT,
+    "banner" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "mediaId" TEXT,
+    "project_owner" TEXT,
+    "project_upvote" TEXT,
+    "groupsId" TEXT,
+    "achievementId" TEXT,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+)
+
+-- CreateTable
+CREATE TABLE "AccountOnCommunity" (
+    "communityId" TEXT NOT NULL,
+    "accountId" TEXT NOT NULL,
+    "assignedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "AccountOnCommunity_pkey" PRIMARY KEY ("communityId","accountId")
+)
+
+-- CreateTable
+CREATE TABLE "Groups" (
+    "id" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "communityId" TEXT NOT NULL,
+    "profile" TEXT,
+    "banner" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Groups_pkey" PRIMARY KEY ("id")
+)
+
+-- CreateTable
+CREATE TABLE "Post_Types" (
+    "id" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+
+    CONSTRAINT "Post_Types_pkey" PRIMARY KEY ("id")
+)
+
+-- CreateTable
+CREATE TABLE "Reaction" (
+    "id" TEXT NOT NULL,
+    "reaction" TEXT NOT NULL,
+    "affectorId" INTEGER NOT NULL,
+    "accountId" TEXT NOT NULL,
+    "commentId" TEXT,
+    "postId" TEXT,
+
+    CONSTRAINT "Reaction_pkey" PRIMARY KEY ("id")
+)
+
+-- CreateTable
+CREATE TABLE "Comment" (
+    "id" TEXT NOT NULL,
+    "comment" TEXT NOT NULL,
+    "affectorId" INTEGER NOT NULL,
+    "accountId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "postId" TEXT,
+
+    CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
+)
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "typeId" TEXT NOT NULL,
+    "posted_by" TEXT NOT NULL,
+    "projectId" TEXT,
+    "achievementId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+)
+
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" TEXT NOT NULL,
+    "communityId" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+)
+
+-- CreateTable
+CREATE TABLE "Achievement" (
+    "id" TEXT NOT NULL,
+    "communityId" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Achievement_pkey" PRIMARY KEY ("id")
+)
+
+-- CreateTable
+CREATE TABLE "Memory_Types" (
+    "id" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+
+    CONSTRAINT "Memory_Types_pkey" PRIMARY KEY ("id")
+)
+
+-- CreateTable
+CREATE TABLE "Memory" (
+    "id" TEXT NOT NULL,
+    "memory_Type" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "accountId" TEXT NOT NULL,
+
+    CONSTRAINT "Memory_pkey" PRIMARY KEY ("id")
+)
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email")
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_emailVerificationCode_key" ON "User"("emailVerificationCode")
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_resetPasswordCode_key" ON "User"("resetPasswordCode")
+
+-- AddForeignKey
+ALTER TABLE "Media" ADD CONSTRAINT "Media_media_typeId_fkey" FOREIGN KEY ("media_typeId") REFERENCES "Media_type"("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "Media" ADD CONSTRAINT "Media_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE SET NULL ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "Media" ADD CONSTRAINT "Media_achievementId_fkey" FOREIGN KEY ("achievementId") REFERENCES "Achievement"("id") ON DELETE SET NULL ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_statusId_fkey" FOREIGN KEY ("statusId") REFERENCES "Status"("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "Community" ADD CONSTRAINT "Community_statusId_fkey" FOREIGN KEY ("statusId") REFERENCES "Status"("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "Community" ADD CONSTRAINT "Community_profile_fkey" FOREIGN KEY ("profile") REFERENCES "Media"("id") ON DELETE SET NULL ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "Community" ADD CONSTRAINT "Community_creatorId_fkey" FOREIGN KEY ("creatorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_statusId_fkey" FOREIGN KEY ("statusId") REFERENCES "Status"("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "Role"("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_communityId_fkey" FOREIGN KEY ("communityId") REFERENCES "Community"("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_profile_fkey" FOREIGN KEY ("profile") REFERENCES "Media"("id") ON DELETE SET NULL ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_banner_fkey" FOREIGN KEY ("banner") REFERENCES "Media"("id") ON DELETE SET NULL ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_mediaId_fkey" FOREIGN KEY ("mediaId") REFERENCES "Media"("id") ON DELETE SET NULL ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_project_owner_fkey" FOREIGN KEY ("project_owner") REFERENCES "Project"("id") ON DELETE SET NULL ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_project_upvote_fkey" FOREIGN KEY ("project_upvote") REFERENCES "Project"("id") ON DELETE SET NULL ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_groupsId_fkey" FOREIGN KEY ("groupsId") REFERENCES "Groups"("id") ON DELETE SET NULL ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_achievementId_fkey" FOREIGN KEY ("achievementId") REFERENCES "Achievement"("id") ON DELETE SET NULL ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "AccountOnCommunity" ADD CONSTRAINT "AccountOnCommunity_communityId_fkey" FOREIGN KEY ("communityId") REFERENCES "Community"("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "AccountOnCommunity" ADD CONSTRAINT "AccountOnCommunity_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "Account"("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "Groups" ADD CONSTRAINT "Groups_communityId_fkey" FOREIGN KEY ("communityId") REFERENCES "Community"("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "Groups" ADD CONSTRAINT "Groups_profile_fkey" FOREIGN KEY ("profile") REFERENCES "Media"("id") ON DELETE SET NULL ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "Groups" ADD CONSTRAINT "Groups_banner_fkey" FOREIGN KEY ("banner") REFERENCES "Media"("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "Reaction" ADD CONSTRAINT "Reaction_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "Account"("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "Reaction" ADD CONSTRAINT "Reaction_commentId_fkey" FOREIGN KEY ("commentId") REFERENCES "Comment"("id") ON DELETE SET NULL ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "Reaction" ADD CONSTRAINT "Reaction_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE SET NULL ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "Account"("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE SET NULL ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_typeId_fkey" FOREIGN KEY ("typeId") REFERENCES "Post_Types"("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_posted_by_fkey" FOREIGN KEY ("posted_by") REFERENCES "Account"("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE SET NULL ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_achievementId_fkey" FOREIGN KEY ("achievementId") REFERENCES "Achievement"("id") ON DELETE SET NULL ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "Project" ADD CONSTRAINT "Project_communityId_fkey" FOREIGN KEY ("communityId") REFERENCES "Community"("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "Achievement" ADD CONSTRAINT "Achievement_communityId_fkey" FOREIGN KEY ("communityId") REFERENCES "Community"("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "Memory" ADD CONSTRAINT "Memory_memory_Type_fkey" FOREIGN KEY ("memory_Type") REFERENCES "Memory_Types"("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+-- AddForeignKey
+ALTER TABLE "Memory" ADD CONSTRAINT "Memory_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "Account"("id") ON DELETE RESTRICT ON UPDATE CASCADE
