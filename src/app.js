295b449782777fb3feab3bcdf6821fd3b59617cf
Beulla <beullarugero6@gmail.com>
2023-03-07 14:28:00 +0200

    account controllers

--- src/app.js
+++ src/app.js
@@ -11,6 +11,8 @@ const userRoutes = require("./routes/user.routes.js");
 const postRoutes = require("./routes/posts.routes.js");
 const storyRoutes = require("../src/routes/story.routes");
 const communityRoutes=require("./routes/community.routes.js")
+const accountRoutes=require("./routes/account.routes.js")
+
 //setting up server
 const app = express();
 
@@ -38,6 +40,7 @@ app.use("api/v1/community", communityRoutes)
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/story", storyRoutes);
+app.use("/api/v1/account/community",accountRoutes);
 
 //default page
 app.get("/", (req, res) => {
--- src/controllers/community.controller.js
+++ src/controllers/community.controller.js
@@ -1,32 +1,30 @@
 // Create a new community
 export const createCommunityController = async (req, res) => {
   try {
+    await prisma.community.create({
+      communityName:req.body.communityName,
+      communityVision:req.body.communityVision,
+      communityTerms:req.body.termsAndConditions
+  })
   } catch (error) {
     console.log(error);
     res.status(500).json(error);
   }
 };
 
-// Add a person to a community
-export const addPersonController = async (req, res) => {
-  try {
-  } catch (error) {
-    console.log(error);
-    res.status(500).json(error);
-  }
-};
-
-// Remove a person from community
-export const removePersonController = async (req, res) => {
-  try {
-  } catch (error) {
-    console.log(error);
-    res.status(500).json(error);
-  }
-};
 
 export const getCommunityById = async (req, res) => {
   try {
+    const community=await prisma.community.find({where:{communityId:req.body.communityId}})
+    if(community){
+      res.status(200).json(community)
+    }
+    else{
+      res.status(500).json({
+        "message":"can't find community with this id"
+      }
+      )
+    }
   } catch (error) {
     console.log(error);
     res.status(500).json(error);
